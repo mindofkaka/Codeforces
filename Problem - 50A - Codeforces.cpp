829679c1ac0946965369c42fee36f87b0cb8f9ae
mindofkaka <106789262+mindofkaka@users.noreply.github.com>
2022-07-28 19:27:56 +0700

    Create Problem - 50A - Codeforces.cpp

---
+++ Problem - 50A - Codeforces.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int M, N;
+    
+    cin >> M >> N;
+    
+    int sum = M*N / 2;
+    
+    cout << sum << endl;
+    
+    return 0;
+}
