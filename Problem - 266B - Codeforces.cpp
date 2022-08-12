4026d7e001192c51e279e8502751b79022307b56
mindofkaka <106789262+mindofkaka@users.noreply.github.com>
2022-08-12 13:13:40 +0700

    Create Problem - 266B - Codeforces.cpp

---
+++ Problem - 266B - Codeforces.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n, t;
+    cin >> n >> t;
+    string s;
+    cin >> s;
+    
+    while(t--){
+        for(int i = 1; i < n; ++i){
+            if(s[i] == 'G' && s[i-1] == 'B'){
+                s[i] = 'B';
+                s[i-1] = 'G';
+                ++i;
+            }
+        }
+    }
+    cout << s << endl;
+    return 0;
+}
