18049b75fbbfe639d17e05e392470f54cdce9aac
mindofkaka <106789262+mindofkaka@users.noreply.github.com>
2022-08-12 14:49:58 +0700

    Create Problem - 344A - Codeforces.cpp

---
+++ Problem - 344A - Codeforces.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    
+    int arr[n];
+    int i;
+    for(i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+    
+    int sum = 0;
+    
+    for(int i = 0; i < n; i++){
+        if(arr[i] != arr[i + 1]){
+            sum++;
+        }
+    }
+    cout << sum;
+}
