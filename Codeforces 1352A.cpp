d799b81ba32a212010f30ebee51ec5eebd9d1c5f
mindofkaka <106789262+mindofkaka@users.noreply.github.com>
2022-08-18 09:39:29 +0700

    Add files via upload

---
+++ Codeforces 1352A.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+string drawZeros(int n){
+    string res="";
+    for(int i=0;i<n;i++){
+        res += "0";
+    }
+    return res;
+}
+
+int main(){
+    int t;
+    string s;
+    cin >> t;
+    while(t--){
+        cin >> s;
+        int ans=0;
+        for(int i=0;i<s.length();i++){
+        	if(s[i]!='0'){
+        		ans++;
+			}
+		}
+        cout << ans << endl;
+        reverse(s.begin(), s.end());
+        for(int i=0;i<s.length();i++){
+            if(s[i]!='0'){
+                cout << s[i] << drawZeros(i) << " ";
+            }
+        }
+        cout << endl;
+    }
+}
