6d38d2f64afa930e7202177d657bb78fd2cb5362
Muhammad Ludaka Firdaus <106789262+mindofkaka@users.noreply.github.com>
2022-10-09 11:35:05 +0700

    Create A. Vus the Cossack and a Contest.cpp

---
+++ A. Vus the Cossack and a Contest.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int n, m, k;
+    cin >> n >> m >> k;
+    if(m < n || k < n) cout << "No";
+    else cout << "Yes";
+}
