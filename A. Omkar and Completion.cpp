ae8e758ba3e6cde40eb742881a63b8dacf28b8d8
Muhammad Ludaka Firdaus <106789262+mindofkaka@users.noreply.github.com>
2022-11-08 16:35:17 +0700

    Add files via upload

---
+++ A. Omkar and Completion.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	int t; cin >> t;
+	while(t--){
+		int n; cin >> n;
+		for(int i = 0; i < n; i++){
+			cout << "5 ";
+		}
+		cout << "\n";
+	}
+}
