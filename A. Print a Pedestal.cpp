c295ed9c59d9b9b73a366b640f3b00383176a67f
Muhammad Ludaka Firdaus <106789262+mindofkaka@users.noreply.github.com>
2022-11-08 21:06:15 +0700

    Add files via upload

---
+++ A. Print a Pedestal.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(){
+	int n; cin >> n;
+	for(int a = 3; a < n; a++){
+		int c = (n-a)/2;
+		int b = n-a-c;
+		if(c > 1 && b+1 < a){
+			c--;
+			b++;
+		}
+		if(a > b && b > c){
+			cout << b << " " << a << " " << c << "\n";
+			break;
+		}
+	}
+}
+
+int main() {
+	int t; cin >> t;
+	while(t--){
+		solve();
+	}
+}
