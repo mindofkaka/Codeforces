77ea38b27622ecbee23c98229d18a68ef3629bd6
Muhammad Ludaka Firdaus <106789262+mindofkaka@users.noreply.github.com>
2022-11-13 20:03:08 +0700

    Create A. cAPS lOCK.cpp

---
+++ A. cAPS lOCK.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string s; cin >> s;
+    bool istrue = true;
+    for(int i = 1; i < s.length(); i++){
+        if(s[i] >= 97){
+            istrue = false;
+            break;
+        }
+    }
+    if(istrue){
+        for(int i = 0; i < s.length(); i++){
+            if(s[i] <= 'Z'){
+                s[i]+=32;
+            }else{
+                s[i]-=32;
+            }
+        }
+        cout << s;
+    }else{
+        cout << s;
+    }
+}
