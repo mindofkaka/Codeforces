801c1f604015cc25a9b87c9e5dfbaeb4bb8df1ad
Muhammad Ludaka Firdaus <106789262+mindofkaka@users.noreply.github.com>
2022-11-16 16:00:36 +0700

    Create A. Sum of Odd Integers.cpp

---
+++ A. Sum of Odd Integers.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int t; cin >> t;
+    while(t--){
+        ll n, k;
+        cin >> n >> k;
+        if(n%2 == k%2){
+            if(k*k <= n) cout << "YES\n";
+            else cout << "NO\n";
+        }else{
+            cout << "NO\n";
+        }
+    }
+}
