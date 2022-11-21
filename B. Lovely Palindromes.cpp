f3d1a0e131a68581bffc12acbdd79410228c29f7
Muhammad Ludaka Firdaus <106789262+mindofkaka@users.noreply.github.com>
2022-11-21 14:58:14 +0700

    Create B. Lovely Palindromes.cpp

---
+++ B. Lovely Palindromes.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	string s; cin >> s;
+	for(int i = s.length()-1; i >= 0; i--){
+		s += s[i];
+	}
+	cout << s;
+}
