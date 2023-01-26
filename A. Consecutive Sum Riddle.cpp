685e263fb230563b14cde6481ec232cf05f50288
Muhammad Ludaka Firdaus <106789262+mindofkaka@users.noreply.github.com>
2023-01-26 21:00:47 +0700

    Create A. Consecutive Sum Riddle.cpp

---
+++ A. Consecutive Sum Riddle.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main() {
+	ll t; cin >> t;
+	while(t--){
+		ll n; cin >> n;
+		cout << -1*(n-1) << " " << n << '\n';
+	}
+}
