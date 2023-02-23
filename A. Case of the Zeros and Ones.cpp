48fe2f91e651c8b8f3f3f1630c21c41a583c6c7a
Muhammad Ludaka Firdaus <106789262+mindofkaka@users.noreply.github.com>
2023-02-23 21:52:37 +0700

    Create A. Case of the Zeros and Ones.cpp

---
+++ A. Case of the Zeros and Ones.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int n; cin >> n;
+	string s; cin >> s;
+	int ans = 0;
+	for(int i = 0; i < n; i++){
+		if(s[i] == '1'){
+			ans++;
+		}else{
+			ans--;
+		}
+	}
+	cout << abs(ans*1);
+}
