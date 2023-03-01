7c2cc5ab292bd6848b9f71f32e3f73aadd515cc7
Muhammad Ludaka Firdaus <106789262+mindofkaka@users.noreply.github.com>
2023-03-01 11:21:46 +0700

    Create A. Regular Bracket Sequence.cpp

---
+++ A. Regular Bracket Sequence.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t; cin >> t;
+	while(t--){
+		string s; cin >> s;
+		if(s.length()%2 == 0 && s[0] != ')' && s[n-1] != '('){
+			cout << "YES\n";
+		}else{
+			cout << "NO\n";
+		}
+	}
+}
